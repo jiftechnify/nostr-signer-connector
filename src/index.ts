ae7255362d5e34b60236987cd06bd63860a6c412
jiftechnify <jiftech.stlfy@gmail.com>
2024-01-13 16:44:33 +0900

    add listenConnection method to support "started by client" flow

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export type { NostrSigner } from "./interface";
 export { Nip07ExtensionSigner, type Nip07Extension } from "./nip07";
 export { Nip46RemoteSigner } from "./nip46";
+export type { Nip46ClientMetadata, Nip46ConnectionParams, Nip46SessionState } from "./nip46";
 export { SecretKeySigner } from "./secret_key";
